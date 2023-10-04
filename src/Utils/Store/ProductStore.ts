24b8e42166792e35856d554cd510313c4a4b8d2e
yasinmhmmdzd <yasinmohammadzade85@gmail.com>
2023-10-04 22:23:22 +0330

    fix(Utils/Store): Create store with zustand

---
+++ src/Utils/Store/ProductStore.ts
@@ -0,0 +1,29 @@
+import { create } from "zustand";
+
+type ProductState ={
+    id:string , 
+    title : string , 
+    price : string , 
+}
+
+type CartState = {
+    cartState: ProductState[]; 
+}
+
+type ProductActions = {
+    addProduct : (product:ProductState) => void
+    addToCart : (product:ProductState) => void 
+    deleteCart : (productId:ProductState['id']) => void 
+}
+
+const useProductsStore = create<CartState & ProductActions>((set) => ({
+    id:'',
+    title: '',
+    price : '',
+    cartState:[],
+    addProduct : (product) => set((state)=>({cartState:[...state.cartState , product]})),
+    addToCart : (product) => set((state) => ({cartState: [...state.cartState , product]})), 
+    deleteCart : (productId) => set((state) => ({cartState: state.cartState.filter(product => product.id !== productId)})) 
+}))
+
+export default useProductsStore;
