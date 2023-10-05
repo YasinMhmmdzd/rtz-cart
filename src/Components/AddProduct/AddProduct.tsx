abc7eac6dcf41c672c4a7149e187637736d1cd41
yasinmhmmdzd <yasinmohammadzade85@gmail.com>
2023-10-05 17:50:44 +0330

    fix(Components/AddProducts): Add status

--- src/Components/AddProduct/AddProduct.tsx
+++ src/Components/AddProduct/AddProduct.tsx
@@ -1,6 +1,24 @@
 import "./AddProduct.css"
 
+import useProductsStore from "../../Utils/Store/ProductStore"
+import { useState } from "react"
+
 function AddProduct(){
+
+    const addProductFunc = useProductsStore((state) => state.addProduct)
+    const [productTitle , setProductTitle] = useState('')
+    const [productPrice , setProdcutPrice] = useState('')
+    const [status , setStatus] = useState('') 
+    
+
+    const addProduct = (e:Event) =>{
+        e.preventDefault();
+        addProductFunc({id:"1" , title:productTitle , price:productPrice})
+
+        setProductTitle("")
+        setProdcutPrice("")
+        setStatus("success")
+    }
 
     return(
 
@@ -9,11 +27,16 @@ function AddProduct(){
                 Add new product
             </h3>
 
-            <form className="add-product-form">
+            <form className="add-product-form" onSubmit={addProduct}>
 
-                <input type="text" placeholder="Product title"/>
-                <input type="text" placeholder="product price"/>
+                <input type="text" value={productTitle} onChange={(e)=> setProductTitle(e.target.value)} placeholder="Product title"/>
+                <input type="text" value={productPrice} onChange={(e)=> setProdcutPrice(e.target.value)} placeholder="product price"/>
                 <button type="submit">Add</button>
+                {
+                    status === "success" && (
+                        <p style={{color:'green'}}>Product Added successfull :)</p>
+                    )
+                }
 
             </form>
         </div>
